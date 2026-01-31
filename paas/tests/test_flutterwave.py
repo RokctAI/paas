6241866639cc78db4fba8340567f1c65d4d484be
Rendani Sinyage <sinyage@gmail.com>
2026-01-31 16:13:55 +0200

    Update test_flutterwave.py

--- paas/tests/test_flutterwave.py
+++ paas/tests/test_flutterwave.py
@@ -2,6 +2,7 @@
 # For license information, please see license.txt
 import frappe
 from frappe.tests.utils import FrappeTestCase
+from unittest.mock import patch, MagicMock
 from paas.api import initiate_flutterwave_payment, flutterwave_callback
 
 class TestFlutterwave(FrappeTestCase):
