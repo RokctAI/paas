f588a61f9979d4dc1d4e9e19c22a46328fc32861
Rendani Sinyage <sinyage@gmail.com>
2026-02-04 04:04:06 +0200

    Create test_admin_logistics.py

---
+++ paas/tests/test_admin_logistics.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2025 ROKCT INTELLIGENCE (PTY) LTD
+# For license information, please see license.txt
+
+import frappe
+from frappe.tests.utils import FrappeTestCase
+from paas.api.admin_logistics.admin_logistics import (
+    get_deliveryman_global_settings, update_deliveryman_global_settings,
+    create_parcel_order_setting, get_parcel_order_settings,
+    create_delivery_vehicle_type, get_delivery_vehicle_types
+)
+
+class TestAdminLogistics(FrappeTestCase):
+    def setUp(self):
+        frappe.set_user("Administrator")
+        # Ensure Settings Doc exists
+        if not frappe.db.exists("DeliveryMan Settings", "DeliveryMan Settings"):
+            frappe.get_doc({"doctype": "DeliveryMan Settings"}).insert(ignore_permissions=True)
+
+    def tearDown(self):
+        frappe.db.rollback()
+
+    def test_deliveryman_settings(self):
+        get_deliveryman_global_settings()
+        update_deliveryman_global_settings({"max_orders": 5})
+        self.assertEqual(frappe.db.get_single_value("DeliveryMan Settings", "max_orders"), 5)
+
+    def test_parcel_order_settings(self):
+        # Create
+        setting = create_parcel_order_setting({"type": "Test Type", "price": 10})
+        self.assertEqual(setting['type'], "Test Type")
+        
+        # Get
+        settings = get_parcel_order_settings()
+        self.assertTrue(len(settings) > 0)
+
+    def test_vehicle_type(self):
+        # Create
+        vtype = create_delivery_vehicle_type({"name": "Bike", "base_fare": 10})
+        self.assertEqual(vtype['name'], "Bike")
+        
+        # Get
+        types = get_delivery_vehicle_types()
+        self.assertTrue(len(types) > 0)
