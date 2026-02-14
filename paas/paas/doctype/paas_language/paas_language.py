b5c4980e63d71641f0eab84f1d9edb835ac6baa4
Rendani Sinyage <sinyage@gmail.com>
2026-02-14 23:55:58 +0200

    Update paas_language.py

--- paas/paas/doctype/paas_language/paas_language.py
+++ paas/paas/doctype/paas_language/paas_language.py
@@ -8,4 +8,9 @@ class PaaSLanguage(Document):
     def validate(self):
         if self.default:
             # Ensure only one default language exists
-            frappe.db.sql("""UPDATE `tabPaaS Language` SET `default` = 0 WHERE name != %s""", self.name)
+            t_paas_lang = frappe.qb.DocType("PaaS Language")
+            (
+                frappe.qb.update(t_paas_lang)
+                .set(t_paas_lang.default, 0)
+                .where(t_paas_lang.name != self.name)
+            ).run()
