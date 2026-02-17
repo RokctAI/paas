d1c4529b0a5e792603b8078e36f0a2128b158380
Rendani Sinyage <sinyage@gmail.com>
2026-02-17 17:37:29 +0200

    Update test_api_paypal.py

--- paas/tests/test_api_paypal.py
+++ paas/tests/test_api_paypal.py
@@ -3,7 +3,7 @@
 import frappe
 from frappe.tests.utils import FrappeTestCase
 from unittest.mock import patch, Mock
-from paas.api import initiate_paypal_payment, handle_paypal_callback
+from paas.api.payment.payment import initiate_paypal_payment, handle_paypal_callback
 
 class TestPayPalAPI(FrappeTestCase):
     def setUp(self):
