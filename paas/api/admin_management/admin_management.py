61f6403b871ad1d15d9049bfb4099bd72252830a
Rendani Sinyage <sinyage@gmail.com>
2026-02-21 00:31:01 +0200

    Update admin_management.py

--- paas/api/admin_management/admin_management.py
+++ paas/api/admin_management/admin_management.py
@@ -2,6 +2,7 @@ import frappe
 import json
 from ..utils import _require_admin
 
+
 @frappe.whitelist()
 def get_all_shops(limit_start: int = 0, limit_page_length: int = 20):
     """
@@ -14,6 +15,7 @@ def get_all_shops(limit_start: int = 0, limit_page_length: int = 20):
         offset=limit_start,
         limit=limit_page_length
     )
+
 
 @frappe.whitelist()
 def get_all_roles(limit_start: int = 0, limit_page_length: int = 20):
